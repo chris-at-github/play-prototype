49788bfa5138f4a2609f0133a85631d6e4fb86c0
Chris <pschorr.christian@gmail.com>
2017-08-22 19:21:27 +0200

    Init

---
+++ index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="description" content="Play package for web and TYPO3 development">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Play</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+	<link rel="stylesheet" href="build/css/screen.css">
+</head>
+<body>
+
+<div class="container--outer">
+	<div class="container--inner container--default container--space-bottom-none">
+		<p>
+			Brownie biscuit cake cake. Lemon drops wafer fruitcake marzipan sweet chocolate. Oat cake macaroon marshmallow
+			jelly-o cheesecake wafer cake jujubes sweet roll. Bonbon pudding sweet roll carrot
+			cake pudding. Wafer donut wafer. Jelly-o jujubes croissant chocolate soufflé sesame snaps cupcake toffee bear
+			claw. Muffin cheesecake icing bonbon fruitcake pudding pudding marzipan pie.
+			Cheesecake chocolate bar dragée liquorice chocolate bar. Sesame snaps powder macaroon soufflé sugar plum jelly
+			beans jelly-o chupa chups pudding.
+		</p>
+	</div>
+</div>
+
+<div class="container--outer">
+	<div class="container--inner container--default container--white">
+		<p>
+			Gingerbread chocolate sesame snaps sugar plum pastry topping caramels. Pastry candy canes tootsie roll cookie
+			marshmallow cookie sweet roll. Cotton candy lemon drops cake. Gummies jujubes sweet
+			gummi bears sweet pastry chupa chups. Brownie cookie carrot cake pudding apple pie chupa chups cupcake lollipop
+			fruitcake. Cake cookie icing gingerbread bear claw gummies. Marshmallow powder
+			wafer dragée cake. Fruitcake soufflé powder candy canes jujubes. Ice cream jelly pie halvah cake danish.
+		</p>
+		<p>
+			Dessert cheesecake tart pudding dragée. Sweet ice cream dessert macaroon icing halvah candy canes. Chocolate bar
+			sesame snaps tart tootsie roll bonbon cake liquorice. Candy chupa chups sugar
+			plum carrot cake chocolate bar. Cake toffee bonbon biscuit chocolate cake. Fruitcake chocolate bonbon biscuit
+			liquorice dragée soufflé.
+		</p>
+	</div>
+</div>
+
+<div class="container--outer">
+	<div class="container--inner">
+
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="container--default container--white">
+					<p>
+						Brownie biscuit cake cake. Lemon drops wafer fruitcake marzipan sweet chocolate. Oat cake macaroon
+						marshmallow
+						jelly-o cheesecake wafer cake jujubes sweet roll. Bonbon pudding sweet roll carrot
+						cake pudding. Wafer donut wafer. Jelly-o jujubes croissant chocolate soufflé sesame snaps cupcake toffee
+						bear.
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="container--default">
+					<p>
+						Muffin cheesecake icing bonbon fruitcake pudding pudding marzipan pie.
+						Cheesecake chocolate bar dragée liquorice chocolate bar. Sesame snaps powder macaroon soufflé sugar plum
+						jelly
+						beans jelly-o chupa chups pudding.
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="container--default container--blue">
+					<p>
+						Brownie biscuit cake cake. Lemon drops wafer fruitcake marzipan sweet chocolate. Oat cake macaroon
+						marshmallow
+						jelly-o cheesecake wafer cake jujubes sweet roll. Bonbon pudding sweet roll carrot
+						cake pudding. Wafer donut wafer. Jelly-o jujubes croissant chocolate soufflé sesame snaps cupcake toffee
+						bear
+						claw.
+					</p>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+<div class="container--outer">
+	<div class="container--inner container--default container--white">
+		<form>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Email address</label>
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPassword1">Password</label>
+				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			</div>
+			<div class="form-group">
+				<label for="exampleInputFile">File input</label>
+				<input type="file" id="exampleInputFile">
+				<p class="help-block">Example block-level help text here.</p>
+			</div>
+			<div class="checkbox">
+				<label>
+					<input type="checkbox"> Check me out
+				</label>
+			</div>
+			<button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</div>
+</div>
+
+</body>
+</html>
